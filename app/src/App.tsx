18ac4087b5c9575b67df5a2668737ae7b35842de
asivath <asivatha@purdue.edu>
2024-10-08 22:28:04 -0400

    added basic home page with search and navbar

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,30 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { SearchBar, NavBar } from "./components";
+import { Box } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <Box display="flex" flexDirection="column" alignItems="center" height="100vh" width="100%">
+      <Box position="sticky" top={0} width="100%" zIndex={1000}>
+        <NavBar />
+      </Box>
+      <Box display="flex" flex="1" alignItems="center" justifyContent="center" width="50%">
+        <SearchBar onSearch={(searchValue) => console.log(searchValue)} />
+      </Box>
+    </Box>
   );
 }
 
