6e9a791db81ce342915831cc7eb3818eba2c93a8
Changk820 <changk7973@gmail.com>
2024-10-09 19:45:28 -0400

    Added more potential licenses and to also go trhough readme file

--- package-rater/src/metrics/License.ts
+++ package-rater/src/metrics/License.ts
@@ -8,38 +8,78 @@ const logger = getLogger();
 const git = simpleGit();
 
 const compatibilityTable = new Map([
-  ["LGPL-2.1", 1],
+  ["LGPL-2.1", 0.75],
   ["MIT", 1],
-  ["GPL-3.0", 0],
+  ["GPL-3.0", 0.25],
   ["Apache-2.0", 1],
   ["BSD-3-Clause", 1],
   ["BSD-2-Clause", 1],
   ["MPL-2.0", 0.5],
-  ["AGPL-3.0", 0],
-  ["EPL-1.0", 0],
-  ["EPL-2.0", 0],
+  ["AGPL-3.0", 0.25],
+  ["EPL-1.0", 0.5],
+  ["EPL-2.0", 0.5],
   ["CC0-1.0", 1],
   ["Unlicense", 1],
   ["ISC", 1],
   ["Zlib", 1],
-  ["Artistic-2.0", 1],
+  ["Artistic-2.0", 0.75],
   ["OFL-1.1", 1],
-  ["EUPL-1.2", 0],
-  ["LGPL-3.0", 1],
-  ["GPL-2.0", 0],
-  ["GPL-2.0+", 0],
-  ["GPL-3.0+", 0],
-  ["AGPL-3.0+", 0],
-  ["LGPL-2.1+", 1],
-  ["LGPL-3.0+", 1],
-  ["Apache-1.1", 0],
-  ["Apache-1.0", 0],
+  ["EUPL-1.2", 0.5],
+  ["LGPL-3.0", 0.75],
+  ["GPL-2.0", 0.25],
+  ["GPL-2.0+", 0.25],
+  ["GPL-3.0+", 0.25],
+  ["AGPL-3.0+", 0.25],
+  ["LGPL-2.1+", 0.75],
+  ["LGPL-3.0+", 0.75],
+  ["Apache-1.1", 0.5],
+  ["Apache-1.0", 0.5],
   ["CC-BY-4.0", 1],
-  ["CC-BY-SA-4.0", 0.5],
+  ["CC-BY-SA-4.0", 0.75],
   ["CC-BY-NC-4.0", 0],
   ["CC-BY-ND-4.0", 0],
   ["CC-BY-NC-SA-4.0", 0],
-  ["CC-BY-NC-ND-4.0", 0]
+  ["CC-BY-NC-ND-4.0", 0],
+  ["0BSD", 1],
+  ["Academic Free License v3.0", 1],
+  ["AFL-3.0", 1],
+  ["Artistic License 2.0", 0.75],
+  ["Boost Software License 1.0", 1],
+  ["BSL-1.0", 1],
+  ["BSD-4-Clause", 0.75],
+  ["BSD-3-Clause-Clear", 1],
+  ["Creative Commons license family", 1],
+  ["CC", 1],
+  ["Creative Commons Zero v1.0 Universal", 1],
+  ["Creative Commons Attribution 4.0", 1],
+  ["Creative Commons Attribution ShareAlike 4.0", 0.75],
+  ["Do What The F*ck You Want To Public License", 1],
+  ["WTFPL", 1],
+  ["Educational Community License v2.0", 0.75],
+  ["ECL-2.0", 0.75],
+  ["Eclipse Public License 1.0", 0.5],
+  ["Eclipse Public License 2.0", 0.5],
+  ["European Union Public License 1.1", 0.5],
+  ["EUPL-1.1", 0.5],
+  ["GNU Affero General Public License v3.0", 0.25],
+  ["GNU General Public License v2.0", 0.25],
+  ["GNU General Public License v3.0", 0.25],
+  ["GNU Lesser General Public License v2.1", 0.75],
+  ["GNU Lesser General Public License v3.0", 0.75],
+  ["LaTeX Project Public License v1.3c", 0.75],
+  ["LPPL-1.3c", 0.75],
+  ["Microsoft Public License", 0.5],
+  ["MS-PL", 0.5],
+  ["Mozilla Public License 2.0", 0.5],
+  ["Open Software License 3.0", 0.5],
+  ["OSL-3.0", 0.5],
+  ["PostgreSQL License", 1],
+  ["PostgreSQL", 1],
+  ["SIL Open Font License 1.1", 0.75],
+  ["University of Illinois/NCSA Open Source License", 1],
+  ["NCSA", 1],
+  ["The Unlicense", 1],
+  ["zLib License", 1],
 ]);
 
 /**
@@ -65,6 +105,8 @@ async function parseLicenseScore(owner: string, repo: string): Promise<number> {
   }
   const licenseFilePath = path.join(dir, "LICENSE");
   const packageFilePath = path.join(dir, "package.json");
+  const readmeFilePath = path.join(dir, "README.md");
+  
   try {
     const packageJson = JSON.parse(await fs.readFile(packageFilePath, "utf8"));
     const packageJsonLicense = packageJson.license;
@@ -77,6 +119,8 @@ async function parseLicenseScore(owner: string, repo: string): Promise<number> {
       logger.error(`Failed to read package.json for ${owner}/${repo}:`, error);
     }
   }
+  
+  // Check License File
   try {
     const licenseLine = (await fs.readFile(licenseFilePath, "utf8")).split("\n")[0].trim();
     for (const [license, score] of compatibilityTable) {
@@ -88,6 +132,21 @@ async function parseLicenseScore(owner: string, repo: string): Promise<number> {
   } catch (error) {
     if ((error as NodeJS.ErrnoException).code !== "ENOENT") {
       logger.error(`Failed to read LICENSE or package.json for ${owner}/${repo}:`, error);
+    }
+  }
+
+  // Check the README file for license information
+  try {
+    const readmeContent = await fs.readFile(readmeFilePath, "utf8");
+    for (const [license, score] of compatibilityTable) {
+      if (readmeContent.toLowerCase().includes(license.toLowerCase())) {
+        logger.info(`Found license ${license} in README for ${owner}/${repo}`);
+        return score;
+      }
+    }
+  } catch (error) {
+    if ((error as NodeJS.ErrnoException).code !== "ENOENT") {
+      logger.error(`Failed to read README.md for ${owner}/${repo}:`, error);
     }
   }
   logger.info(`No license found for ${owner}/${repo}`);
